2208d241566d1b7c34188ebd69ec62e00464dc20
mjohanik <mateuszjohanik2@gmail.com>
2022-04-10 19:45:57 +0100

    initial add composition project

---
+++ newo-core-composition/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace newo_core_composition
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
